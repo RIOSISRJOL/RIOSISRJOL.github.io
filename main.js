3031e936253eaa83641e8d60d8ddc5630f4109bf
RIOSISRJOL <158153577+RIOSISRJOL@users.noreply.github.com>
2024-01-30 14:58:36 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: false,
+      arrows: false,
+      infinite: true,
+    });
+});
